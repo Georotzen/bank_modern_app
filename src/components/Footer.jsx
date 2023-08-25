8ca355381e4d27434edebdce5800645c2fdedfe1
unknown <mptoce@gmail.com>
2023-08-25 14:36:41 -0400

    finished footer section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => (
       {/* div for logo and paragraph */}
       <div className='flex-1 flex flex-col justify-start mr-10'>
         <img src={logo} alt='hoobank' className='w-[266px] h-[72px] object-contain'/>
-        <p className={`${styles.paragraph} mt-4 max-w-[310px]`}>A new way to make the payments easy, reliable and secure.</p>
+        <p className={`${styles.paragraph} ml-6 mt-4 max-w-[310px]`}>A new way to make the payments easy, reliable and secure.</p>
       </div>
       {/* div for links */}
       <div className='flex-[1.5] w-full flex flex-row justify-between flex-wrap md:mt-0 mt-10'>
@@ -29,13 +29,22 @@ const Footer = () => (
     {/* TODO: */}
     {/* div for grey separation bar */}
     
-
+    {/* <hr className='h-[2px] my-4 bg-neutral-800 border-0 w-full' /> */}
     {/* div for copyright */}
-    <div className='flex'>
-      <p className={`${styles.paragraph} pr-2`}>Copyright</p>
-      <img src={copyright} alt='copyright' className='w-[16px] h-[16px] object-contain mt-[7px]'/>
-      <p className={`${styles.paragraph} pl-4`}>2021 HooBank. All Rights Reserved.</p>
-    </div>
+    <div className='flex w-full justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3F3E45]'>
+      <div className='flex'>
+        <p className={`${styles.paragraph} pr-2`}>Copyright</p>
+        <img src={copyright} alt='copyright' className='w-[16px] h-[16px] object-contain mt-[7px]'/>
+        <p className={`${styles.paragraph} pl-4`}>2021 HooBank. All Rights Reserved.</p>
+      </div>
+      {/* div for social media links */}
+      <div className='flex flex-row md:mt-0 mt-6'>
+        {socialMedia.map((social, index) => (
+          <img key={social.id} src={social.icon} alt={social.id} className={`w-[21px] h-[21px] object-contain cursor-pointer ${index !== socialMedia.length - 1 ? 'mr-6' : 'mr-0' }`}/>
+        ))}
+      </div>
+    </div>    
+
   </section>
 )
 
